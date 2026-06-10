d42d57760edc8bdcfcfd5b2438173ee1beed24da
Ashish Kumar <ashishjangra206@gmail.com>
2026-06-10 17:38:16 +0530

    Init Commit 10-06-2026

---
+++ src/main/java/com/lsb/searchservice/controller/SearchController.java
@@ -0,0 +1,60 @@
+package com.lsb.searchservice.controller;
+
+import com.lsb.searchservice.client.FileSearchClient;
+import com.lsb.searchservice.client.FolderSearchClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/search")
+@RequiredArgsConstructor
+public class SearchController {
+
+    private final FileSearchClient fileSearchClient;
+    private final FolderSearchClient folderSearchClient;
+
+    @GetMapping
+    public Map<String, Object> search(@RequestParam String query) {
+
+        List<Map<String, Object>> allFiles = fileSearchClient.getAllFiles();
+        List<Map<String, Object>> folders = folderSearchClient.getAllFolders();
+
+        List<Map<String, Object>> fileResult = allFiles.stream()
+                .filter(f -> f.get("name").toString().toLowerCase().contains(query.toLowerCase()))
+                .toList();
+
+        List<Map<String, Object>> folderResult = folders.stream()
+                .filter(f -> f.get("name").toString().toLowerCase().contains(query.toLowerCase()))
+                .toList();
+
+        Map<String, Object> result = new HashMap<>();
+        result.put("files", fileResult);
+        result.put("folders", folderResult);
+        return result;
+    }
+
+    @GetMapping("/files")
+    public List<Map<String, Object>> searchFiles(@RequestParam String query) {
+        List<Map<String, Object>> allFiles = fileSearchClient.getAllFiles();
+        return allFiles.stream()
+                .filter(f -> f.get("name").toString().toLowerCase().contains(query.toLowerCase()))
+                .toList();
+    }
+
+    @GetMapping("/folders")
+    public List<Map<String, Object>> searchFolder(@RequestParam String query) {
+        List<Map<String, Object>> folders = folderSearchClient.getAllFolders();
+        return folders.stream()
+                .filter(f -> f.get("name").toString().toLowerCase().contains(query.toLowerCase()))
+                .toList();
+
+    }
+
+}
---
+++ src/test/java/com/lsb/searchservice/SearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsb.searchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
